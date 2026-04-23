36bc0f5f56946cdbb644f3fcd6ae3c442c211fd6
Valentin Mikhalchuk <valy378@gmail.com>
2026-04-23 18:25:08 +0800

    fix

--- ChemHazard.lean
+++ ChemHazard.lean
@@ -1,3 +1,7 @@
+
+/-
+  Please, open the file `ChemHazard/Basic.lean` to see the basic definitions of substances, mixtures, and protocols
+-/
 -- This module serves as the root of the `ChemHazard` library.
 -- Import modules here that should be built as part of the library.
 import ChemHazard.Basic
