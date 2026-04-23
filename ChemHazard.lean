7fecce77521bf567f5881fa59c87b3c7318c9a6c
Lean 4 VS Code Extension <>
2026-04-23 18:17:30 +0800

    Initial commit

---
+++ ChemHazard.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `ChemHazard` library.
+-- Import modules here that should be built as part of the library.
+import ChemHazard.Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import ChemHazard
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
